8749796732f983da96041d6239ef952ec4f0303f
Ana Maria Studler <42396007+anastudler@users.noreply.github.com>
2024-02-29 23:07:05 -0300

    Created Todos and Todo components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,31 @@
-
+import { useState } from 'react'
+import { Todos } from './components/Todos';
+const mockTodos = [
+  {
+    id: 1,
+    title: 'New Todo',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'Todo',
+    completed: true
+  },
+  {
+    id: 3,
+    title: 'Todo 3',
+    completed: false
+  }
+]
 
 const App = (): JSX.Element => {
+  const [todos] = useState(mockTodos);
+
+
   return (
-    <h1>To do</h1>
+    <div className='todoapp'>
+      <Todos todos={todos} />
+    </div>
   )
 }
 
